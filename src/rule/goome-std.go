5cee2ff7878433da03f5a62c062ac60db30f21cc
장진의 <je_chang@motrex.co.kr>
2016-01-05 20:53:05 +0900

    crc check 수정중

--- src/rule/goome-std.go
+++ src/rule/goome-std.go
@@ -13,14 +13,24 @@ import (
 type GoomeStd struct {
 }
 
-func (re *GoomeStd) Parse(dataLength int, rawdata []byte, conn net.Conn, IMEIMap map[net.Conn]string) []Message{
+func (re *GoomeStd) Parse(rawdataLength int, rawdata []byte, conn net.Conn, IMEIMap map[net.Conn]string) []Message{
 
-	msg := new(Message)
-
+//	msg := new(Message)
 	msgList := make([]Message, 0)
 
-//	for _, data := range bytes.Split(rawdata[:dataLength], []byte{0x0d, 0x0a}){
-//		if data != nil && len(data) != 0{
+	for _, data := range bytes.SplitAfter(rawdata[:rawdataLength], []byte{0x0d, 0x0a}){
+		if(bytes.HasPrefix(data, []byte{0x78, 0x78})){
+			/*  check packageLength */
+			if(int(data[2]) == len(data)-5){
+				/* check CRC */
+				i := util.Crc16(data[2:len(data)-4])
+				var h, l uint8 = uint8(i>>8), uint8(i&0xff)
+				if h == uint8(data[len(data)-4]) && l == uint8(data[len(data)-3]) {
+					fmt.Println("111111111111")
+				}
+			}
+		}
+//		if len(data) != 0{
 //			data = bytes.TrimPrefix(data, []byte{0x78, 0x78})
 //			if (len(data)-1) == int(data[0]){
 //				msg = parseGoomeRawData(data, msg)
@@ -32,33 +42,34 @@ func (re *GoomeStd) Parse(dataLength int, rawdata []byte, conn net.Conn, IMEIMap
 //				msgList = append(msgList, *msg)
 //			}
 //		}
+	}
+
+
+
+//	var startPoint int = -1
+//	var endPoint int
+//
+//	for i := 0; i < dataLength; i++{
+//		if rawdata[i] == 0x78 && rawdata[i+1] == 0x78{
+//			startPoint = i
+//		}else if rawdata[i] == 0x0D && rawdata[i+1] == 0x0A{
+//			endPoint = i+2
+//			if startPoint != -1 && (endPoint - startPoint) > 14 && (endPoint - startPoint) < 45{
+//				msg = parseGoomeRawData(rawdata[startPoint:endPoint], msg)
+//				if value, ok := IMEIMap[conn]; ok {
+//					msg.IMEI = value
+//				}else{
+//					IMEIMap[conn] = msg.IMEI
+//				}
+//				msgList = append(msgList, *msg)
+//			}
+//		}
 //	}
 
-
-	var startPoint int = -1
-	var endPoint int
-
-	for i := 0; i < dataLength; i++{
-		if rawdata[i] == 0x78 && rawdata[i+1] == 0x78{
-			startPoint = i
-		}else if rawdata[i] == 0x0D && rawdata[i+1] == 0x0A{
-			endPoint = i+2
-			if startPoint != -1 && (endPoint - startPoint) > 14 && (endPoint - startPoint) < 45{
-				msg = parseGoomeRawData(rawdata[startPoint:endPoint], msg)
-				if value, ok := IMEIMap[conn]; ok {
-					msg.IMEI = value
-				}else{
-					IMEIMap[conn] = msg.IMEI
-				}
-				msgList = append(msgList, *msg)
-			}
-		}
-	}
-
-	if (dataLength == 15 && rawdata[3] == 0x13) ||
-		(dataLength == 18 && rawdata[3] == 0x01) {
-		responseGoomeData(rawdata, dataLength, conn)
-	}
+//	if (dataLength == 15 && rawdata[3] == 0x13) ||
+//		(dataLength == 18 && rawdata[3] == 0x01) {
+//		responseGoomeData(rawdata, dataLength, conn)
+//	}
 
 	return msgList
 }
